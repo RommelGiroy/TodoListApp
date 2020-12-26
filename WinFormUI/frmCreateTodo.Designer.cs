396e794e964cd9868ad1a3364036ac7dfcfa8158
Rommel Giroy <giroyrommel03@gmail.com>
2020-12-26 13:53:14 +0800

    Added frmTodoInformation form and update frmCreateTodo form

--- WinFormUI/frmCreateTodo.Designer.cs
+++ WinFormUI/frmCreateTodo.Designer.cs
@@ -31,12 +31,12 @@ namespace WinFormUI
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCreateTodo));
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnDaily = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnWeekly = new System.Windows.Forms.Button();
+            this.btnMonthly = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -49,20 +49,20 @@ namespace WinFormUI
             this.label1.TabIndex = 0;
             this.label1.Text = "Create Todo List";
             // 
-            // button1
+            // btnDaily
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.BackColor = System.Drawing.SystemColors.Control;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(379, 109);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(345, 75);
-            this.button1.TabIndex = 1;
-            this.button1.Text = " Daily Todo List\r\n";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnDaily.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDaily.BackColor = System.Drawing.SystemColors.Control;
+            this.btnDaily.FlatAppearance.BorderSize = 0;
+            this.btnDaily.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDaily.Image = ((System.Drawing.Image)(resources.GetObject("btnDaily.Image")));
+            this.btnDaily.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDaily.Location = new System.Drawing.Point(379, 109);
+            this.btnDaily.Name = "btnDaily";
+            this.btnDaily.Size = new System.Drawing.Size(345, 75);
+            this.btnDaily.TabIndex = 1;
+            this.btnDaily.Text = " Daily Todo List\r\n";
+            this.btnDaily.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
@@ -74,14 +74,14 @@ namespace WinFormUI
             this.label2.TabIndex = 2;
             this.label2.Text = "Open recent";
             // 
-            // textBox1
+            // txtSearch
             // 
-            this.textBox1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(24, 109);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PlaceholderText = " Search";
-            this.textBox1.Size = new System.Drawing.Size(317, 33);
-            this.textBox1.TabIndex = 3;
+            this.txtSearch.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSearch.Location = new System.Drawing.Point(24, 109);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.PlaceholderText = " Search";
+            this.txtSearch.Size = new System.Drawing.Size(317, 33);
+            this.txtSearch.TabIndex = 3;
             // 
             // label3
             // 
@@ -93,50 +93,51 @@ namespace WinFormUI
             this.label3.TabIndex = 4;
             this.label3.Text = "Get started";
             // 
-            // button2
+            // btnWeekly
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.BackColor = System.Drawing.SystemColors.Control;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(379, 199);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(345, 75);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Weekly Todo List";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnWeekly.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnWeekly.BackColor = System.Drawing.SystemColors.Control;
+            this.btnWeekly.FlatAppearance.BorderSize = 0;
+            this.btnWeekly.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWeekly.Image = ((System.Drawing.Image)(resources.GetObject("btnWeekly.Image")));
+            this.btnWeekly.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnWeekly.Location = new System.Drawing.Point(379, 199);
+            this.btnWeekly.Name = "btnWeekly";
+            this.btnWeekly.Size = new System.Drawing.Size(345, 75);
+            this.btnWeekly.TabIndex = 5;
+            this.btnWeekly.Text = "Weekly Todo List";
+            this.btnWeekly.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // btnMonthly
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.BackColor = System.Drawing.SystemColors.Control;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(379, 292);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(345, 75);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Monthly Todo List";
-            this.button3.UseVisualStyleBackColor = false;
+            this.btnMonthly.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMonthly.BackColor = System.Drawing.SystemColors.Control;
+            this.btnMonthly.FlatAppearance.BorderSize = 0;
+            this.btnMonthly.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMonthly.Image = ((System.Drawing.Image)(resources.GetObject("btnMonthly.Image")));
+            this.btnMonthly.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMonthly.Location = new System.Drawing.Point(379, 292);
+            this.btnMonthly.Name = "btnMonthly";
+            this.btnMonthly.Size = new System.Drawing.Size(345, 75);
+            this.btnMonthly.TabIndex = 6;
+            this.btnMonthly.Text = "Monthly Todo List";
+            this.btnMonthly.UseVisualStyleBackColor = false;
             // 
             // frmCreateTodo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(745, 386);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(745, 521);
+            this.Controls.Add(this.btnMonthly);
+            this.Controls.Add(this.btnWeekly);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDaily);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "frmCreateTodo";
             this.ShowIcon = false;
@@ -149,11 +150,11 @@ namespace WinFormUI
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDaily;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSearch;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnWeekly;
+        private System.Windows.Forms.Button btnMonthly;
     }
 }
